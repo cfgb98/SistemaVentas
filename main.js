97b5c96a92be48e0901310cb19c7317587808bf9
lizeth211 <61747627+lizeth211@users.noreply.github.com>
2021-01-27 13:10:21 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ function validar(){
     let usario = document.getElementById('user');
     let pwd = document.getElementById('password');
     let err = document.getElementById('error');
+    let numPedido =  document.getElementById('numpedido');
+    let idcliente =  document.getElementById('idcliente');
+    let cliente =  document.getElementById('cliente');
+    let idproducto =  document.getElementById('idproducto');
+    let producto =  document.getElementById('producto');
+    let cantidad =  document.getElementById('cantidad');
+    let importe =  document.getElementById('importe');
+
     err.style.color='black';
     let mensajesErr =[];
 
@@ -13,7 +21,84 @@ if(usario.value==null || usario.value==''){
         mensajesErr.push('ingresa tu contraseña');//agregar valor
     }
 
+    if(numPedido.value==null || numPedido.value==''){
+        mensajesErr.push('ingresa número de pedido');
+    }
+
+    if(idcliente.value==null || idcliente.value==''){
+        mensajesErr.push('ingresa idcliente');
+    }
+
+    if(cliente.value==null || cliente.value==''){
+        mensajesErr.push('ingresa cliente');
+    }
+
+    if(idproducto.value==null || idproducto.value==''){
+        mensajesErr.push('ingresa idproducto');
+    }
+
+    if(producto.value==null || producto.value==''){
+        mensajesErr.push('ingresa producto');
+    }
+
+    if(cantidad.value==null || cantidad.value==''){
+        mensajesErr.push('ingresa cantidad');
+    }
+
+    if(importe.value==null || importe.value==''){
+        mensajesErr.push('ingresa importe');
+    }
+
   mensajesErr.join(',');//separar errores por ,
     err.innerHTML=mensajesErr;
     return false;
 }
+
+function soloNum(e){
+    let key =e.keyCode || e.which;
+    let teclado= String.fromCharCode(key);
+    let err = document.getElementById('error');
+    err.style.color='black';
+    let mensajesErr =[];
+    let numeros="0123456789";
+    let especiales="8-37-38-46";//ascii
+    let telcado_especial=false;
+    
+    for (let i in especiales) {
+        if (key==especiales[i]){
+            telcado_especial=true;
+        }
+        }
+        //-1 si no se encontro
+        if(numeros.indexOf(teclado)==-1 && !telcado_especial){
+            mensajesErr.push('Solo números');
+            mensajesErr.join(',');//separar errores por ,
+             err.innerHTML=mensajesErr;
+            return false;
+        }
+    }
+    
+    function soloLetras(e){
+        let key =e.keyCode || e.which;
+        let teclado= String.fromCharCode(key).toLowerCase();
+        let letras ="abcdefghijklmnñopqrstuvwxyz";
+        let especiales="8-37-38-46-164";
+        let telcado_especial=false;
+        let err = document.getElementById('error');
+        err.style.color='black';
+        let mensajesErr =[];
+    
+        for (let i in especiales) {
+            if (key==especiales[i]){
+                telcado_especial=true;
+                break;
+            }
+            }
+            if (letras.indexOf(teclado)==-1 && !telcado_especial){
+                mensajesErr.push('Solo letras');
+                mensajesErr.join(',');//separar errores por ,
+                err.innerHTML=mensajesErr;
+                return false;
+            }
+    
+    }
--- pedidos.php
+++ pedidos.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8"/>
 		
 		 <link rel="stylesheet" type="text/css" href="style.css" />
+		 <script src="main.js"></script>
 		 <title>Pedidos de Venta</title>
 	
     </head>
@@ -16,17 +17,18 @@
 <form name="pedidos" class="login-form" action="generarxml.php" method="post">
 		<div class="content">
 		<br>
-		<input name="numpedido" type="text" class="input indata" placeholder="Número de Pedido: 5000" required>
-		<input name="idcliente" type="text" class="input indata" placeholder="Número de Cliente: 1000" required>
-		<input name="cliente" type="text" class="input indata" placeholder="Cliente: Bimbo" required>
-		<input name="idproducto" type="text" class="input indata" placeholder="Número de Producto: 3000" required>
-		<input name="producto" type="text" class="input indata" placeholder="Producto: Mesas" required>
-		<input name="cantidad" type="text" class="input indata" placeholder="Cantidad: 10" required>
-		<input name="importe" type="text" class="input indata" placeholder="Importe: 1200.00" required>
+		<input name="numpedido"  id="numpedido" onkeypress="return soloNum(event)" type="text" class="input indata" placeholder="Número de Pedido: 5000" required>
+		<input name="idcliente" id="idcliente" type="text" onkeypress="return soloNum(event)" class="input indata" placeholder="Número de Cliente: 1000" required>
+		<input name="cliente" id="cliente" type="text" onkeypress="return soloLetras(event)" class="input indata" placeholder="Cliente: Bimbo" required>
+		<input name="idproducto" id="idproducto" onkeypress="return soloNum(event)" type="text" class="input indata" placeholder="Número de Producto: 3000" required>
+		<input name="producto" id="producto" onkeypress="return soloLetras(event)" type="text" class="input indata" placeholder="Producto: Mesas" required>
+		<input name="cantidad" id="cantidad" onkeypress="return soloNum(event)" type="text" class="input indata" placeholder="Cantidad: 10" required>
+		<input name="importe" id="importe" onkeypress="return soloNum(event)" type="text" class="input indata" placeholder="Importe: 1200.00" required>
 		</div>
 		<div class="footer">
-		<input type="submit" name="login" value="Generar XML" class="button" />
+		<input type="submit" name="login" value="Generar XML" class="button"  onClick="return validar()"/>
 		</div>
 		</form>
+		<div id="error"></div>
 </body>
 </html> 
